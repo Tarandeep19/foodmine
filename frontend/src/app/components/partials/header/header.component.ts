96ea2ad769f644b230583bc953798399ae145ed5
Tarandeep19 <tarandeep.kohli18@gmail.com>
2025-03-25 13:27:52 +0530

    first commit

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,19 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CartService } from '../../../services/cart.service';
+import { User } from '../../../shared/models/User';
+import { UserService } from '../../../services/user.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink,NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
   cartQuantity=0;
-   constructor(cartService:CartService) {
-     cartService.getCartObservable().subscribe((newCart) => {
+  user!:User;
+  constructor(cartService:CartService,private userService:UserService) {     
+    cartService.getCartObservable().subscribe((newCart) => {
        this.cartQuantity = newCart.totalCount;
      })
+
+     userService.userObservable.subscribe((newUser) => {
+      this.user = newUser;
+    })
+    }
+
+    logout(){
+      this.userService.logout();
+    }
+  
+    get isAuth(){
+      return this.user.token;
     }
 }
